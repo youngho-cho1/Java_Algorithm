357731dece918769ad6386fc18c3fd57cae574fd
youngho <mmy4637@naver.com>
2023-03-10 23:19:15 +0900

    Update 최소 힙.java

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,34 +1,28 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException   {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());// 1~n
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> q = new PriorityQueue<>((o1,o2) ->{
-			int one = Math.abs(o1);
-			int two = Math.abs(o2);
-			if(one == two) {
-				return o1 > o2 ? 1: -1; // 절댓값이 같은 경우 음수 우선
-			}
-			return one - two; // 절댓값 작은 데이터 우선
-		});
-		
-		for(int i=0; i<N; i++) {
-			int M = Integer.parseInt(br.readLine());
-			if(M == 0) {
-				if(q.isEmpty()) {
-					System.out.println("0");
-				}else System.out.println(q.poll());
-			}else {
-				q.add(M);
-			}
-		}
-	}
-
+        int input;
+        PriorityQueue<Integer> Q = new PriorityQueue<Integer>();
+        for (int i = 0; i < n; i++) {
+            input = Integer.parseInt(br.readLine());
+            if (input>0) {
+                Q.add(input);
+            } else {
+                if (!Q.isEmpty()) {
+                    System.out.println(Q.poll());
+                }
+                else{
+                    System.out.println(0);
+                }
+            }
+        }
+        br.close();
+    }
 }
