8b5841ece6934d45b312f259299a9d2b97db840f
youngho <mmy4637@naver.com>
2023-03-20 18:50:48 +0900

    Update 소수 구하기.java

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,35 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        primeNumber(m, n);
-    }
-    
-    //에라토스테네스의 체 이용
-    static void primeNumber(int m, int n) {
-        int[] arr = new int[n+1];
-        StringBuilder sb = new StringBuilder();
-        
-        //배열 초기화
-        for (int i = 2; i <= n; i++) {
-            arr[i] = i;
-        }
-        
-        //2부터 시작해서 i의 배수들을 배열에서 지워준다
-        for (int i = 2; i <= n; i++) {
-            //이미 지워진 수는 건너뛴다
-            if (arr[i] == 0) continue;  
-            for (int j = i+i; j <= n; j += i) {
-                arr[j] = 0;
-            }
-        }
-        for (int i = m; i <= n; i++) {
-            if (arr[i] != 0) sb.append(i + "\n");
-        }
-        System.out.print(sb);
-    }
+
+	public static void main(String[] args) throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		
+		int A[] = new int[n+1];
+		
+		for(int i=2; i<n+1; i++) {
+			A[i] = i; // 배열 초기화
+		}
+		
+		for(int i=2; i<=n; i++) {
+			if(A[i]==0) continue;
+			for(int j=i+i; j<=n; j+=i) {
+				A[j] = 0;
+			}
+		}
+		for(int i=m; i<=n; i++) {
+			if(A[i]!=0) {
+				System.out.println(A[i]);
+			}
+			
+		}
+	}
+
 }
