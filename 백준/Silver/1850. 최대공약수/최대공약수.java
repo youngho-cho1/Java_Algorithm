5741ec7d2c23df092c4f59d33ad75a43466c93b0
youngho <mmy4637@naver.com>
2023-03-22 07:40:52 +0900

    Update 최대공약수.java

--- 백준/Silver/1850. 최대공약수/최대공약수.java
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -15,6 +15,7 @@ public class Main {
 		
 		long gcd = GCD(a,b);
 		
+		// 문제에서 입력한 두 숫자의 최대공약수를 구하고 그 수만큼 1을 출력
 		for(int i=0; i<gcd; i++) sb.append("1");
 //		for(int i=0; i<gcd; i++) System.out.print("1"); // 시간초과
 		
@@ -23,8 +24,9 @@ public class Main {
 
 	private static long GCD(long a, long b) {
 		// TODO Auto-generated method stub
+		//b의 값이 0이면 a를 그대로 리턴
 		if(b == 0) return a;
-		else {
+		else { 
 			long r = a%b;
 			return GCD(b,r);
 		}
